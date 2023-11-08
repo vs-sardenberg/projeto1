286619583726f9f24fe99fdb10beed30e888e897
Vanessa Sardenberg <vds.sardenberg@gmail.com>
2023-11-08 10:20:57 -0300

    first commit

---
+++ src/test/java/natan/io/projeto1/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package natan.io.projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
